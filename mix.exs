5b8d75d6bd8a7c207f1b2c1c7c816eb46b804358
Alexei Sholik <alcosholik@gmail.com>
2017-01-27 12:40:00 +0100

    Fix Elixir 1.4 warnings

--- mix.exs
+++ mix.exs
@@ -7,27 +7,15 @@ defmodule ConformExrm.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     package: package,
+     package: package(),
      description: "Conform plugin for ExRM",
-     deps: deps]
+     deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: []]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:exrm, "> 0.0.0"},
      {:conform, "> 0.0.0"}]
